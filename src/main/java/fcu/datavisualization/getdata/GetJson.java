61540fbe7328430db7598aab7806aee154205ef9
黎嘉修 <Eason@Eason-MacBook-Pro.local>
2016-12-14 01:45:43 +0800

    可抓到日期 解碼日期字串 轉成日期物件比較是否遲交 也可透過ＪＳＰ將值呈現在圖表上

--- src/main/java/fcu/datavisualization/getdata/GetJson.java
+++ src/main/java/fcu/datavisualization/getdata/GetJson.java
@@ -7,9 +7,14 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 public class GetJson {
 
@@ -27,29 +32,73 @@ public class GetJson {
     
     try {
     	
- 	
     	BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-        String jsonText = readAll(rd);
-        jsonText = "{output:" + jsonText + "}";
-        //System.out.println("字串");
-        //System.out.println(jsonText);
+        String jsonText = readAll(rd);        
+        JSONArray output = new JSONArray(jsonText);
+        //System.out.println(output.getJSONObject(0).getJSONObject("commit").getJSONObject("author").getString("date"));
+        return output.getJSONObject(0).getJSONObject("commit").getJSONObject("author");
         
-        /*while((rd.readLine()) != null){
-        	jsonText.replace("[", " ").replace("]", " ");
-        }*/
-        
-        JSONObject json = new JSONObject(jsonText);
-        Object o = json.get("output");
-        return json;
       } finally {
         is.close();
-      }	
-    	
+      }	  	
   }
+  
+  public int GetDate(String Day) throws IOException, JSONException, ParseException{
+	    JSONObject json = readJsonFromUrl("https://api.github.com/repos/fcu-d0271888/WP-HW1/commits");
+	    String date = (String) json.getString("date").subSequence(0, 10);  //日期
+	    String time = (String) json.getString("date").subSequence(11, 19); //時間
+	    String DateTime = date + " " + time;
+	    System.out.println(date);
+	    System.out.println(time);
+	    System.out.println(DateTime);
+	    System.out.println("時間");
+	    System.out.println(json.getString("date"));
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    Date a = sdf.parse(DateTime);
+	    Date c = sdf.parse(Day);
+	    
+	    
+	    if(a.getTime() > c.getTime()){
+	    	System.out.println("遲交");
+	    	return 1;
+	    }
+	    else{
+	    	System.out.println("在期限內繳交");
+	    	return 0;
+	    }
+	  }
+  
+  /*public static Date transform(String Day) throws ParseException{
+	  SimpleDateFormat sdfa = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	  return sdfa.parse(Day);
+  }*/
+  
 
-  public static void main(String[] args) throws IOException, JSONException {
-    JSONObject json = readJsonFromUrl("https://api.github.com/repos/FoodOrder/Eclipse/commits");
-    System.out.println("JSON");
-    System.out.println(json.toString());
-  }
+  /*public static void main(String[] args) throws IOException, JSONException, ParseException {
+    JSONObject json = readJsonFromUrl("https://api.github.com/repos/fcu-d0271888/WP-HW1/commits");
+    String date = (String) json.getString("date").subSequence(0, 10);  //日期
+    String time = (String) json.getString("date").subSequence(11, 19); //時間
+    String DateTime = date + " " + time;
+    System.out.println(date);
+    System.out.println(time);
+    System.out.println(DateTime);
+    System.out.println("時間");
+    System.out.println(json.getString("date"));
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date a = sdf.parse(DateTime); 
+    Date b = transform("2016-09-21 15:32:55");
+    System.out.println("**" + transform("2016-09-21 15:32:55"));
+    
+    int ontime = 0;
+    int late = 0;
+    
+    if(a.getTime() > b.getTime()){
+    	System.out.println("遲交");
+    	late++;
+    }
+    else{
+    	System.out.println("在期限內繳交");
+    	ontime++;
+    }
+  }*/
 }
--- src/main/webapp/Chart.jsp
+++ src/main/webapp/Chart.jsp
@@ -1,41 +1,54 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    <% int a = 20; %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+    <%@page import = "fcu.datavisualization.getdata.GetJson"%>
+    <% 
+    GetJson GetDay = new GetJson();
+    int OntimeCount = 0;
+    int LateCount = 0;
+    int output = 3;
+    output = GetDay.GetDate("2016-09-21 15:32:55");
+    if(output == 1){
+    	LateCount++;
+    }
+    else if(output == 0){
+    	OntimeCount++;
+    }
+    %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-	var b = <%=a%>;
+      google.charts.setOnLoadCallback(drawVisualization);
+
+
+      function drawVisualization() {
+        // Some raw data (not necessarily accurate)
+        var LateCount = <%=LateCount%>;
+        var OntimeCount = <%=OntimeCount%>;
         var data = google.visualization.arrayToDataTable([
-          ['時間', '人數'],
-          ['10',  b],
-          ['20',  0],
-          ['30',  1],
-          ['40',  0],
-	  	  ['50',  0],
-          ['60',  2],
-          ['70',  0],
-          ['80',  0],
-	 	  ['90',  2],
-	      ['100', 1]
-        ]);
+         ['Month', '已交作業', '遲交作業', '未交作業'],
+         ['09/20',  LateCount,      OntimeCount,          3],
+         ['12/06',  35,      1,          5],
+         ['12/20',  28,      2,         12],
+         ['01/08',  38,      10,         2],
+         ['01/12',  25,      3,         15]
+      ]);
 
-        var options = {
-          title: '成績分布圖',
-          hAxis: {title: '分數',  titleTextStyle: {color: '#333'}},
-          vAxis: {minValue: 0}
-        };
+    var options = {
+      title : '作業狀況',
+      vAxis: {title: '人數'},
+      hAxis: {title: '日期'},
+      seriesType: 'bars',
+      series: {5: {type: 'line'}}
+    };
 
-        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+    var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
     </script>
   </head>
   <body>
-    <div id="chart_div" style="width: 100%; height: 500px;"></div>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
   </body>
 </html>
